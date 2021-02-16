4e6d5ba0e2901e274787ff981df6f5391c370914
davidmarsal <davidmarsal@iesebre.com>
2021-02-16 17:25:47 +0100

    sync

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,7 +4,6 @@
       <div class="flex flex-col">
         <div>
           Item added successfully</div>
-
         <div>
           <button @click="close" class="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"> CLOSE </button>
         </div>
